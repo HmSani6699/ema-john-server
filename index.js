671069197bb3733e725ec7455b733b7cf9dd3f39
H.M Sadiq <hmsadikurrahman120@gmail.com>
2023-05-29 21:19:06 +0600

    add to mongo db callaction

--- index.js
+++ index.js
@@ -28,8 +28,14 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
+    const prodectsCollaction =client.db('prodectsDB').collection('prodects');
 
-    
+    app.get('/prodects',async(req,res)=>{
+        const prodects = await prodectsCollaction.find().toArray();
+        res.send(prodects)
+    })
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
